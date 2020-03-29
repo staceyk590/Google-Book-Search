56d58208eac3be3dcdf75940ce9004c59cf76464
Stacey Koumaras <stacey_k@tpg.com.au>
2020-03-29 12:42:15 +1100

    homework

---
+++ client/build/precache-manifest.d8f7cd167fe52ced2f392781d8952884.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "067abda9eb99f5015b78",
+    "url": "/static/css/main.408fc083.chunk.css"
+  },
+  {
+    "revision": "067abda9eb99f5015b78",
+    "url": "/static/js/main.285d98fd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5bb399ca0583b2b2daab",
+    "url": "/static/js/2.46e23993.chunk.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "fcda2dd6e459efa737ed41853a4a7a8b",
+    "url": "/index.html"
+  }
+];
